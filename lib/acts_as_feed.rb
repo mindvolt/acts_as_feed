69bd8ac28ea69d771733e98ba740c2920d48f5bf
Chris McLeod <chris@mindvolt.com>
2013-07-22 16:40:25 -0500

    Updated to delete all duplicates instead of just the first one

--- lib/acts_as_feed.rb
+++ lib/acts_as_feed.rb
@@ -50,8 +50,7 @@ module ActsAsFeed
   	end
 
     def acts_as_feed_rebuild!
-      feed = ActsAsFeed::Feed.where(:feedable_id => self.id, :feedable_type => self.class.to_s).first
-      feed.delete unless feed.nil?
+      ActsAsFeed::Feed.delete_all(:feedable_id => self.id, :feedable_type => self.class.to_s)
       ActsAsFeed::Feed.create({
         :updated_at => self.send(self.class.acts_as_feed_on),
         :feedable => self
